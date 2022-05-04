4f4fdd7965ffdcc65f0f8f57f28519b36c385b3a
Adam Davidson <adamgd91@gmail.com>
2022-05-03 20:10:45 -0500

    added wireframe

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,9 +1,9 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 
+import AxiosOnlineStatus from "../Routes/status";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
-import AxiosOnlineStatus from "../Routes/status";
 
 const AuthContext = createContext();
 
@@ -30,7 +30,7 @@ export const AuthProvider = ({ children }) => {
       if (response.status === 200) {
         let token = response.headers["x-auth-token"];
         localStorage.setItem("token", JSON.stringify(token));
-        console.log(token)
+        console.log(token);
         setUser(jwtDecode(token));
         navigate("/");
       } else {
@@ -45,10 +45,10 @@ export const AuthProvider = ({ children }) => {
     try {
       let response = await axios.post(`${BASE_URL}/login`, loginData);
       if (response.status === 200) {
-        console.log(response)
+        console.log(response);
         localStorage.setItem("token", JSON.stringify(response.data));
         setUser(jwtDecode(response.data));
-        console.log(jwtDecode(response.data))
+        console.log(jwtDecode(response.data));
         setIsServerError(false);
         navigate("/");
       } else {
@@ -69,9 +69,8 @@ export const AuthProvider = ({ children }) => {
       }
       navigate("/");
       localStorage.removeItem("token");
-      console.log("token removed")
+      console.log("token removed");
       setUser(null);
-
     }
   };
 
